d6ddada8ffc6c32e62ed7f76e3644cff3ca1b525
Hanasolo <hanasolo@hotmail.com>
2024-04-23 15:18:41 +0200

    starting the proyect

---
+++ 03.PROYECTO.Generador-Refranes/resources/js/index.js
@@ -0,0 +1,19 @@
+/*la idea principal tomar aleatoriamente  de una lista de refranes dos refranes y mezclarlos en uno nuevo 
+
+Componentes:
+------------
+
+lista de refranes 
+selecionador de refran 1
+seleccionador de refran 2
+mezclador 
+
+
+boton 
+
+output con el nuevo refran
+
+(mirar node js y algo de machine learning lo primero)
+
+
+*/
